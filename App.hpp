b64885d72456af07b200d8098aa0b0d059d16ad2
ramin <pool@localhost.localdomain>
2016-05-09 22:54:28 -0700

    Fixed indentation to be consistent among tabs.

--- App.hpp
+++ App.hpp
@@ -12,68 +12,65 @@
 
 class App
 {
-public:
-  
-  App()
-  {
-    //driver=NULL;
-    //smgr=NULL;
-    //guienv=NULL;
-    //eReceiver=NULL;
-
-  }
-
-  // Initializes and sets main pointers.
-  App(int argc, char *argv[]) 
-  {
-		      //MyEventReceiver eReceiverObj;	
-		//eReceiver=&eReceiverObj;
-		
-  	      eReceiver = new MyEventReceiver(); 
-		
-		//eReceiver.init();
-
-		//TODO: Place handler code in seperate function (necassary?)
-  	      device = irr::createDeviceEx(processCMDArgs(argc,argv));
-		soundEngine = irrklang::createIrrKlangDevice();
-		soundEngine->play2D("../../irrKlang-64bit-1.5.0/media/getout.ogg",1,0);
-		soundEngine->play2D("../../irrKlang-64bit-1.5.0/media/explosion.wav",1,0);
-
-		if (!device)
-		{
-		//	exit 1;
-		}
-
-  	      device->setEventReceiver(eReceiver);
-		device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
-
-		driver = device->getVideoDriver();
-		smgr = device->getSceneManager();
-		guienv = device->getGUIEnvironment();
-
-
-		guienv->addStaticText(L" \"T\" to type. ESC to close.",
-		irr::core::rect<irr::s32>(10,10,260,22), true);
-
-  }
-
-  ~App()
-  {
-
-    delete eReceiver;
-    guienv->drop();
-    device->drop();
-    //smgr->drop();
-  }
-
-
-  void runLoop()
-  {
-	  std::cout << "kik\n";
-
+	public:
+	  
+	  App()
+	  {
+	    //driver=0;
+	    //smgr=0;
+	    //guienv=0;
+	    //eReceiver=0;
+	
+	  }
+	
+	  // Initializes and sets main pointers.
+	  App(int argc, char *argv[]) 
+	  {
+			      //MyEventReceiver eReceiverObj;	
+			//eReceiver=&eReceiverObj;
+			
+			eReceiver = new MyEventReceiver(); 
+			
+			//eReceiver.init();
+	
+			//TODO: Place handler code in seperate function (necassary?)
+			device = irr::createDeviceEx(processCMDArgs(argc,argv));
+			soundEngine = irrklang::createIrrKlangDevice();
+			soundEngine->play2D("../../irrKlang-64bit-1.5.0/media/getout.ogg",1,0);
+			soundEngine->play2D("../../irrKlang-64bit-1.5.0/media/explosion.wav",1,0);
+	
+			if (!device)
+			{
+				exit(1);
+			}
+	
+			device->setEventReceiver(eReceiver);
+			device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
+	
+			driver = device->getVideoDriver();
+			smgr = device->getSceneManager();
+			guienv = device->getGUIEnvironment();
+	
+	
+			guienv->addStaticText(L" \"T\" to type. ESC to close.",
+			irr::core::rect<irr::s32>(10,10,260,22), true);
+	
+	  }
+	
+	~App()
+	{
+		delete eReceiver;
+		guienv->drop();
+		device->drop();
+		//smgr->drop();
+	}
+	
+	
+	void runLoop()
+	{
 		irr::u32 then = device->getTimer()->getTime();
-
-    while(device->run())
+		
+		while(device->run())
 		{
 			if (device->isWindowActive())
 			{
@@ -81,77 +78,75 @@ public:
 				deltaTime = now - then;
 				then = now;
 				lag += deltaTime;
-					
+									
 				// locks game logic to LOGIC_MS_PER_UPDATE.
 				while ( lag >= LOGIC_MS_PER_UPDATE )
 				{
-				  	std::cout << lag << " ";
-      	   	lag -= LOGIC_MS_PER_UPDATE;
+					std::cout << lag << " ";
+					lag -= LOGIC_MS_PER_UPDATE;
 				}
-
+				
 				std::cout << "\n";
-      	                  
-
-				if (RENDER_MS_PER_UPDATE - deltaTime >=0)
+				      	                  
+				
+				if ( RENDER_MS_PER_UPDATE - deltaTime >=0 )
 				{
-				//device->sleep(RENDER_MS_PER_UPDATE);
-				device->sleep(RENDER_MS_PER_UPDATE - deltaTime);
+					//device->sleep(RENDER_MS_PER_UPDATE);
+					device->sleep(RENDER_MS_PER_UPDATE - deltaTime);
 				}
-
-	
-//				eReceiver.startEventProcess();
-
+				
+					
+				//				eReceiver.startEventProcess();
+				
 				//driver->beginScene(true, true, irr::video::SColor(255,100,101,140));
 				driver->beginScene(irr::video::ECBF_COLOR | irr::video::ECBF_DEPTH, irr::video::SColor(255,120,102,136));
-
+				
 				smgr->drawAll();
 				guienv->drawAll();
 				driver->endScene();
 				
 				// Run chatbox.
-
-
+				
 				if (eReceiver->IsKeyDown(irr::KEY_ESCAPE))
 				{
 					//device->closeDevice();
 					device->drop();
 				}
+								
+				//			bool pressed = 0;
+				//				std::cout << eReceiver.getMousePosition().X << "," << eReceiver.getMousePosition().Y << " " ;
 				
-	//			bool pressed = 0;
-
-//				std::cout << eReceiver.getMousePosition().X << "," << eReceiver.getMousePosition().Y << " " ;
-
 				//if (eReceiver->getScrollWheel() == 1) std::cout << "S_UP ";
 				//if (eReceiver->getScrollWheel() == -1) std::cout << "S_DOWN ";
 				//
 				//	std::cout << eReceiver.getScrollWheel() << " " ;
-	
+				
 				//if (eReceiver->IsMousePressed(MMB)) std::cout << "PRESSED ";
 				//if (eReceiver->IsMouseReleased(MMB)) std::cout << "RELEASED ";
 				//if (eReceiver->IsMouseDown(MMB)) std::cout << "DOWN ";
-
+				
 				//if (eReceiver->IsMousePressed(LMB)) std::cout << "PRESSED ";
 				//if (eReceiver->IsMouseReleased(LMB)) std::cout << "RELEASED ";
 				//if (eReceiver->IsMouseDown(LMB)) std::cout << "DOWN ";
-
+				
 				//if (eReceiver->IsMousePressed(RMB)) std::cout << "PRESSED ";
 				//if (eReceiver->IsMouseReleased(RMB)) std::cout << "RELEASED ";
 				//if (eReceiver->IsMouseDown(RMB)) std::cout << "DOWN ";
-
+				
 				//
 				//
 				//if (eReceiver->IsKeyPressed(irr::KEY_LBUTTON)) std::cout << "PRESSED ";
 				//if (eReceiver->IsKeyReleased(irr::KEY_LBUTTON)) std::cout << "RELEASED ";
 				//if (eReceiver->IsKeyDown(irr::KEY_LBUTTON)) std::cout << "DOWN ";
-
+				
 				//if (eReceiver->IsKeyPressed(irr::KEY_RBUTTON)) std::cout << "PRESSED ";
 				//if (eReceiver->IsKeyReleased(irr::KEY_RBUTTON)) std::cout << "RELEASED ";
 				//if (eReceiver->IsKeyDown(irr::KEY_RBUTTON)) std::cout << "DOWN ";
-
+				
 				//if (eReceiver->IsKeyPressed(irr::KEY_MBUTTON)) std::cout << "PRESSED ";
 				//if (eReceiver->IsKeyReleased(irr::KEY_MBUTTON)) std::cout << "RELEASED ";
 				//if (eReceiver->IsKeyDown(irr::KEY_MBUTTON)) std::cout << "DOWN ";
-
+				
 				//if (eReceiver->IsKeyPressed(irr::KEY_KEY_L)) std::cout << "PRESSED ";
 				//if (eReceiver->IsKeyReleased(irr::KEY_KEY_L)) std::cout << "RELEASED ";
 				//if (eReceiver->IsKeyDown(irr::KEY_KEY_L)) std::cout << "DOWN ";
@@ -160,27 +155,27 @@ public:
 				////if (pressed) std::cout << "Mmouse pressed"; pressed =false;
 				////std::cout << eReceiver.getMousePtr()->wheel << " ";
 				//
-//				//eReceiver->endEventProcess();
+				//				//eReceiver->endEventProcess();
 				eReceiver->run();
 			}
 		}
 	}
-  
-private: 
+	  
+	private: 
+		
+	irr::IrrlichtDevice* device = 0;
+	irrklang::ISoundEngine* soundEngine = 0;
+	irr::video::IVideoDriver* driver = 0;
+	irr::scene::ISceneManager* smgr = 0;
+	irr::gui::IGUIEnvironment* guienv = 0;
+		
+	MyEventReceiver* eReceiver = 0;
 	
-	irr::IrrlichtDevice* device = NULL;
-	irrklang::ISoundEngine* soundEngine = NULL;
-	irr::video::IVideoDriver* driver = NULL;
-	irr::scene::ISceneManager* smgr = NULL;
-	irr::gui::IGUIEnvironment* guienv = NULL;
-	
-	MyEventReceiver* eReceiver = NULL;
-
 	double deltaTime = 0.0;
-	int LOGIC_MS_PER_UPDATE = 1000/10; // 10FPS WRITE GETTER/SETTER FOR THESE.
-	int RENDER_MS_PER_UPDATE = 1000/1; // 1FPS
+	double LOGIC_MS_PER_UPDATE = 1000/10; // 10FPS WRITE GETTER/SETTER FOR THESE.
+	double RENDER_MS_PER_UPDATE = 1000/1; // 1FPS
 	double lag = 0.0;
-	
+		
 };
 
 #endif /* APP_H */
--- eventReceiver.h
+++ eventReceiver.h
@@ -13,208 +13,190 @@ class MyEventReceiver : public irr::IEventReceiver
 {
 public:
 
-    MyEventReceiver()
-    {
-        for (irr::u32 i=0; i<irr::KEY_KEY_CODES_COUNT; ++i) 
+	MyEventReceiver()
 	{
-		KeyIsDown[i] = false;
-		KeyWasDown[i] = false;
-		KeyIsPressed[i] = false;
-		KeyIsReleased[i] = false;
+		for (irr::u32 i=0; i<irr::KEY_KEY_CODES_COUNT; ++i) 
+		{
+			KeyIsDown[i] = false;
+			KeyWasDown[i] = false;
+			KeyIsPressed[i] = false;
+			KeyIsReleased[i] = false;
+		}
+		
+		for (irr::u32 i=0; i<MOUSE_CODES_COUNT; ++i) 
+		{
+			MouseIsDown[i] = false;
+			MouseIsPressed[i] = false;
+			MouseIsReleased[i] = false;
+		}
+		
+		mouseWheel = 0;
+		mousePosition = irr::core::position2di(0,0);
 	}
-
-        for (irr::u32 i=0; i<MOUSE_CODES_COUNT; ++i) 
-	{
-		MouseIsDown[i] = false;
-		MouseIsPressed[i] = false;
-		MouseIsReleased[i] = false;
+	
+	 
+	// This is the one method that we have to implement
+	virtual bool OnEvent(const irr::SEvent& event)
+	{   
+		// Remember the mouse state
+		if (event.EventType == irr::EET_MOUSE_INPUT_EVENT)
+		{
+			switch(event.MouseInput.Event)
+			{
+				case irr::EMIE_MOUSE_MOVED:
+					mousePosition.X = event.MouseInput.X;
+					mousePosition.Y = event.MouseInput.Y;
+					break;
+						   
+				case irr::EMIE_MOUSE_WHEEL:
+					mouseWheel = event.MouseInput.Wheel; 
+					break;
+				
+				case irr::EMIE_LMOUSE_PRESSED_DOWN:
+					if (!MouseIsDown[LMB]) MouseIsPressed[LMB] = 1;
+					MouseIsDown[LMB] = 1;
+					MouseWasDown[LMB] = 1;
+					break;
+				 
+				case irr::EMIE_LMOUSE_LEFT_UP:
+					MouseIsDown[LMB]=0;
+					MouseIsReleased[LMB]=0;
+					break;
+				
+				case irr::EMIE_RMOUSE_PRESSED_DOWN:
+					if (!MouseIsDown[RMB]) MouseIsPressed[RMB] = 1; 
+					MouseIsDown[RMB] = 1;
+					MouseWasDown[RMB] = 1;
+					break;
+				 
+				case irr::EMIE_RMOUSE_LEFT_UP:
+					MouseIsDown[RMB]=0;
+					MouseIsReleased[RMB]=0;
+					break;
+				
+				case irr::EMIE_MMOUSE_PRESSED_DOWN:
+					if (!MouseIsDown[MMB]) MouseIsPressed[MMB] = 1; 
+					MouseIsDown[MMB] = 1;
+					MouseWasDown[MMB] = 1;
+					break;
+						     
+				case irr::EMIE_MMOUSE_LEFT_UP:
+					MouseIsDown[MMB]=0;
+					MouseIsReleased[MMB]=0;
+					break;
+				  	 
+				default:
+					break;
+			}
+		}
+		 
+		// Remember whether each key is down or up
+		// trigger pressed only once somehow
+		if (event.EventType == irr::EET_KEY_INPUT_EVENT) 
+		{
+			if (!KeyIsDown[event.KeyInput.Key]) KeyIsPressed[event.KeyInput.Key] = 1;
+			KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
+			KeyWasDown[event.KeyInput.Key] = 1;
+		}
+		return false;
 	}
-
-        mouseWheel = 0;
-        mousePosition = irr::core::position2di(0,0);
-    }
- 
- 
-    // This is the one method that we have to implement
-    virtual bool OnEvent(const irr::SEvent& event)
-    {   
-        // Remember the mouse state
-        if (event.EventType == irr::EET_MOUSE_INPUT_EVENT)
-        {
-
-            switch(event.MouseInput.Event)
-            {
-	    case irr::EMIE_MOUSE_MOVED:
-	        mousePosition.X = event.MouseInput.X;
-	        mousePosition.Y = event.MouseInput.Y;
-                break;
-	   
-	    case irr::EMIE_MOUSE_WHEEL:
-	        mouseWheel = event.MouseInput.Wheel; 
-                break;
-
-	    case irr::EMIE_LMOUSE_PRESSED_DOWN:
-	        if (!MouseIsDown[LMB]) 
-	        {
-	            MouseIsPressed[LMB] = 1;
-                }
-                MouseIsDown[LMB] = 1;
-	        MouseWasDown[LMB] = 1;
-	        break;
- 
-	    case irr::EMIE_LMOUSE_LEFT_UP:
-		MouseIsDown[LMB]=0;
-		MouseIsReleased[LMB]=0;
-		break;
-
-	    case irr::EMIE_RMOUSE_PRESSED_DOWN:
-                if (!MouseIsDown[RMB]) 
-	        {
-	            MouseIsPressed[RMB] = 1;
-                }
-                MouseIsDown[RMB] = 1;
-	        MouseWasDown[RMB] = 1;
-		break;
- 
-	    case irr::EMIE_RMOUSE_LEFT_UP:
-		MouseIsDown[RMB]=0;
-		MouseIsReleased[RMB]=0;
-		break;
-
-	    case irr::EMIE_MMOUSE_PRESSED_DOWN:
-                if (!MouseIsDown[MMB]) 
-	        {
-	            MouseIsPressed[MMB] = 1;
-                }
-                MouseIsDown[MMB] = 1;
-	        MouseWasDown[MMB] = 1;
-		break;
-	     
-	    case irr::EMIE_MMOUSE_LEFT_UP:
-		MouseIsDown[MMB]=0;
-		MouseIsReleased[MMB]=0;
-		break;
-   
-            default:
-                break;
-            }
-        }
- 
-        // Remember whether each key is down or up
-        // trigger pressed only once somehow
-        if (event.EventType == irr::EET_KEY_INPUT_EVENT) 
-	{
-            if (!KeyIsDown[event.KeyInput.Key]) 
-	    {
-	      KeyIsPressed[event.KeyInput.Key] = 1;
-            }
-            KeyIsDown[event.KeyInput.Key] = event.KeyInput.PressedDown;
-	    KeyWasDown[event.KeyInput.Key] = 1;
-        }
-        return false;
-    }
  
     // Check whether a key is being held down
-    virtual bool IsKeyDown(irr::EKEY_CODE keyCode) const
-    {
-        return KeyIsDown[keyCode];
-    }
-    // Receive initial key press.
-    virtual bool IsKeyPressed(irr::EKEY_CODE keyCode) const
-    {
-        return KeyIsPressed[keyCode];
-    }
- 
-    virtual bool IsKeyReleased(irr::EKEY_CODE keyCode) const
-    {
-        return KeyIsReleased[keyCode];
-    }
-
-    virtual bool IsMouseDown(int mouseCode) const
-    {
-        return MouseIsDown[mouseCode];
-    }
- 
-    virtual bool IsMousePressed(int mouseCode) const
-    {
-        return MouseIsPressed[mouseCode];
-    }
- 
-    virtual bool IsMouseReleased(int mouseCode) const
-    {
-        return MouseIsReleased[mouseCode];
-    }
-
-    virtual void run()
-    {
-        mouseWheel = 0.0f;
-	for (irr::u32 i=0; i < MOUSE_CODES_COUNT; ++i)
+	virtual bool IsKeyDown(irr::EKEY_CODE keyCode) const
 	{
-	  if (MouseIsDown[i] && MouseIsPressed[i])
-	  {
-	    MouseIsPressed[i]=0;
-	  }
-
-	  // check if key was pressed last frame.
-	  if (!MouseIsDown[i] && MouseWasDown[i])
-	  {
-            MouseIsReleased[i]=1;
-	    MouseWasDown[i]=0;
-	  }
-	  else
-	  {
-            MouseIsReleased[i]=0;
-	  }
+		return KeyIsDown[keyCode];
+	}
+	// Receive initial key press.
+	virtual bool IsKeyPressed(irr::EKEY_CODE keyCode) const
+	{
+		return KeyIsPressed[keyCode];
+	}
+	virtual bool IsKeyReleased(irr::EKEY_CODE keyCode) const
+	{
+		return KeyIsReleased[keyCode];
+	}
+	virtual bool IsMouseDown(int mouseCode) const
+	{
+		return MouseIsDown[mouseCode];
+	}
+	
+	virtual bool IsMousePressed(int mouseCode) const
+	{
+		return MouseIsPressed[mouseCode];
+	}
+	
+	virtual bool IsMouseReleased(int mouseCode) const
+	{
+		return MouseIsReleased[mouseCode];
 	}
 
-	for (irr::u32 i=0; i < irr::KEY_KEY_CODES_COUNT; ++i)
+	virtual void run()
 	{
-	  if (KeyIsDown[i] && KeyIsPressed[i])
-	  {
-	    KeyIsPressed[i]=0;
-	  }
-
-	  // check if key was pressed last frame.
-	  if (!KeyIsDown[i] && KeyWasDown[i])
-	  {
-            KeyIsReleased[i]=1;
-	    KeyWasDown[i]=0;
-	  }
-	  else
-	  {
-            KeyIsReleased[i]=0;
-	  }
+		mouseWheel = 0.0f;
+		for (irr::u32 i=0; i < MOUSE_CODES_COUNT; ++i)
+		{
+			if (MouseIsDown[i] && MouseIsPressed[i]) 
+				MouseIsPressed[i]=0;
+			
+			// check if key was pressed last frame.
+			if (!MouseIsDown[i] && MouseWasDown[i])
+			{
+				MouseIsReleased[i]=1;
+				MouseWasDown[i]=0;
+			}
+			else
+			{
+				MouseIsReleased[i]=0;
+			}
+		}
+	
+		for (irr::u32 i=0; i < irr::KEY_KEY_CODES_COUNT; ++i)
+		{
+			if (KeyIsDown[i] && KeyIsPressed[i]) 
+				KeyIsPressed[i]=0;
+			
+			// check if key was pressed last frame.
+			if (!KeyIsDown[i] && KeyWasDown[i])
+			{
+				KeyIsReleased[i]=1;
+				KeyWasDown[i]=0;
+			}
+			else
+			{
+				KeyIsReleased[i]=0;
+			}
+		}
 	}
-    }
  
-    irr::core::position2di getMousePosition() 
-    {
-      return mousePosition;
-    }
-    
-    irr::f32 getScrollWheel()
-    {
-      return mouseWheel;
-    }
+	irr::core::position2di getMousePosition() 
+	{
+		return mousePosition;
+	}
+	
+	irr::f32 getScrollWheel()
+	{
+		return mouseWheel;
+	}
+	
     
 private:
-    // We use this array to store the current state of each key
-    bool KeyIsDown[irr::KEY_KEY_CODES_COUNT];
-    bool KeyIsPressed[irr::KEY_KEY_CODES_COUNT];
-    bool KeyIsReleased[irr::KEY_KEY_CODES_COUNT];
-    bool KeyWasDown[irr::KEY_KEY_CODES_COUNT];
-
-
-    //irr::core::map<int,int> pressedKeysMap;
-    //irr::core::map<int,int> releasedKeysMap;
-    
-    bool MouseIsDown[MOUSE_CODES_COUNT];
-    bool MouseIsPressed[MOUSE_CODES_COUNT];
-    bool MouseIsReleased[MOUSE_CODES_COUNT];
-    bool MouseWasDown[MOUSE_CODES_COUNT];
-
-    irr::core::position2di mousePosition;
-    float mouseWheel;
+	// We use this array to store the current state of each key
+	bool KeyIsDown[irr::KEY_KEY_CODES_COUNT];
+	bool KeyIsPressed[irr::KEY_KEY_CODES_COUNT];
+	bool KeyIsReleased[irr::KEY_KEY_CODES_COUNT];
+	bool KeyWasDown[irr::KEY_KEY_CODES_COUNT];
+	
+	
+	//irr::core::map<int,int> pressedKeysMap;
+	//irr::core::map<int,int> releasedKeysMap;
+	
+	bool MouseIsDown[MOUSE_CODES_COUNT];
+	bool MouseIsPressed[MOUSE_CODES_COUNT];
+	bool MouseIsReleased[MOUSE_CODES_COUNT];
+	bool MouseWasDown[MOUSE_CODES_COUNT];
+	
+	irr::core::position2di mousePosition;
+	float mouseWheel;
 };
 
 
