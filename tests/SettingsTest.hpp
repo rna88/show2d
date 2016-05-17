1430d6c5df0a273bbe0df339bae35d0b9624d291
ramin <pool@localhost.localdomain>
2016-05-17 00:35:00 -0700

    Added assert tests for settings file.

--- tests/SettingsTest.hpp
+++ tests/SettingsTest.hpp
@@ -1,22 +1,42 @@
 #include <irrlicht.h>
 #include <iostream>
+#include <assert.h>
 #include "../Settings.hpp"
 
 //int lol = irr::KEY_KEY_CODES_COUNT;
 
 enum { JUMP = 57, RUN = 42 };
 
+irr::core::stringw driverName = "OPENGL";
+irr::core::stringw fullscreen = "0";
+//irr::core::stringw driverName = L"lol";
+
+// Runs tests for the setting object.
 void SettingsTest()
 {
+// note: settings are modified once load is called, but permanent modifications only occur
+// after write is called.
 	Settings testSettings("options.xml");
 	std::cout << "constructing Settings object\n";
+
 	testSettings.load();
 	std::cout << "loading settings\n";
+
+	//assert( driverName == testSettings.getSetting(driverName) );
+	//assert( driverName.c_str() == testSettings.getSetting(driverName).c_str() );
+
 	testSettings.setSetting("lol", "bbbb");
-	std::cout << testSettings.getSettingAsInteger);
-	testSettings.setSetting("resolution", 453534);
+	testSettings.setSetting("resolution", 45);
 	testSettings.save();
 	std::cout << "saving settings\n";
+
+	//testSettings.load();
+	testSettings.save();
+	assert( 45 == testSettings.getSettingAsInteger("resolution") );
+	//assert( driverName.c_str() == testSettings.getSetting("driver").c_str() );
+	assert( 0 == testSettings.getSettingAsInteger("fullscreen"));
+	assert( fullscreen == testSettings.getSetting("fullscreen") );
+	assert( driverName == testSettings.getSetting("driver") );
 
 	//// Should return enums of any pressed action.
 	//testSettings.getAction(JUMP);
